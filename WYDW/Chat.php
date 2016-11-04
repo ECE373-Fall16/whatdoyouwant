73b483e904d03e91a5c1f6cf5de5b242056728f9
Ayodeji Marquis <ayodejimarquis@gmail.com>
2016-11-04 02:13:32 -0400

    simple chatbox implementation

---
+++ WYDW/Chat.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="Styles/chat.css">
+    </head>
+    <body>
+        <div class="chatbox">
+            <div class="chatlogs">
+                <div class="chat">
+                    <p class="chatt-message">What's up, Brother..!</p>
+                </div>
+                    <div class="chat">
+                    <p class="chatt-message">What's up, ..!</p>
+                </div>
+                    <div class="chat">
+                    <p class="chatt-message">What's up, Brother..!</p>
+                </div>
+            </div>
+            <div class="chat-form">
+                <textarea></textarea>
+                <button>Send</button>
+            </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
